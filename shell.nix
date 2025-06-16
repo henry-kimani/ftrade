76a7137199883bf4942ffa963f033209566598ee
kim henry <kimanihenry14489@gmail.com>
2025-06-16 15:36:24 +0300

    Intialized supabase and db pull

---
+++ shell.nix
@@ -0,0 +1,32 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-25.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+  pkgs.mkShellNoCC {
+    packages = with pkgs; [ 
+      supabase-cli
+      podman-compose
+      cowsay
+      lolcat
+    ];
+
+    buildInputs = with pkgs; [
+      # 15.8.1.069
+      postgresql
+    ];
+
+    # Using podman instead of docker
+    DOCKER_HOST="unix:///run/user/1000/podman/podman.sock";
+
+    shellHook = ''
+
+      printf " 
+        FTrade Project Entered 
+        To connect with psql, run
+        psql -h localhost -U postgres -p 'port'
+      " | cowsay | lolcat
+
+      zsh # To enter zsh after completing instead of bash
+    '';
+  }
