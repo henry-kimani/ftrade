8554983b719bfcba5580d85544eea339afa60972
kim henry <kimanihenry14489@gmail.com>
2025-06-16 15:38:02 +0300

    Intialized drizzle orm

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./supabase/migrations",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+  migrations: {
+    prefix: "supabase"
+  }
+});
