ab70b36102a4635fe7e8f1511c4e7f21cb747811
kim henry <kimanihenry14489@gmail.com>
2025-08-16 13:25:00 +0300

    Added the local and prod env variable for connecting to the TCP server

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SUPABASE_URL: string;
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+    NEXT_PUBLIC_MT_SERVER_PORT: number;
 
     INITIAL_ADMIN: string;
     DATABASE_URL: string;
