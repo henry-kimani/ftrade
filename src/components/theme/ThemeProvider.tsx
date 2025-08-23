3b54a5bb7eaee2986b1a736649a2c81cdc4d6187
kim henry <kimanihenry14489@gmail.com>
2025-08-23 22:47:57 +0300

    Fixed hydration error with next-themes

--- src/components/theme/ThemeProvider.tsx
+++ src/components/theme/ThemeProvider.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+const NextThemeProvider = dynamic(() => import ('next-themes').then(e => e.ThemeProvider), { ssr: false })
+
 import React from "react";
-import { ThemeProvider as NextThemeProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes";
+import dynamic from "next/dynamic";
 
 export default function ThemeProvider({
   children,
   ...props
-}: React.ComponentProps<typeof NextThemeProvider>) {
-  return <NextThemeProvider {...props}>{children}</NextThemeProvider>
+}: ThemeProviderProps) {
+  return <NextThemeProvider attribute="class" {...props}>{children}</NextThemeProvider>
 }
 
