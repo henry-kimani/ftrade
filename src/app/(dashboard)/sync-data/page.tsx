d815a58b4b6ebd1d3690bb05e8572e424e756ff9
kim henry <kimanihenry14489@gmail.com>
2025-08-23 22:51:37 +0300

    Secured the sync-data route

--- src/app/(dashboard)/sync-data/page.tsx
+++ src/app/(dashboard)/sync-data/page.tsx
@@ -1,14 +1,24 @@
 import SiteHeader from "@/components/SiteHeader";
 import SyncToMt from "@/components/SyncToMT";
+import { isUserAdmin } from "@/db/queries";
+import { verifyUser } from "@/lib/dal";
 
-export default function SyncData() {
+export default async function SyncData() {
+  const { user } = await verifyUser();
+  const isAdmin = await isUserAdmin(user.id);
+
   return (
     <main>
       <div>
         <SiteHeader heading="sync data" />
       </div>
       <div className="max-w-4xl mx-auto p-4 mt-5">
-        <SyncToMt />
+        {isAdmin ? 
+          <SyncToMt />:
+          <div>
+            <p className="text-xl text-muted-foreground">You are not an admin.</p>
+          </div>
+        }
       </div>
     </main>
   );
